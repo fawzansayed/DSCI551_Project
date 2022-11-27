9386acfe0ec4cb98636e5c316649dd58fd52c8f0
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2022-11-26 19:20:24 -0800

    minor formatting

--- EDFS/ls_firebase.py
+++ EDFS/ls_firebase.py
@@ -3,7 +3,7 @@ import json
 
 url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/root"
 
-inp=input('Enter command: ')
-a=inp.split()
-resp=requests.get(url+a[1]+'.json').json()
+inp = input('Enter command: ')
+a = inp.split()
+resp = requests.get(url + a[1] + '.json').json()
 print(resp)
