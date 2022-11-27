816054963600d84612ac7135f74db5d22cdeba11
JeelMehta04 <jeelmaya@usc.edu>
2022-11-27 13:19:43 -0800

    Hopefully final commit

--- EDFS/rm_firebase.py
+++ EDFS/rm_firebase.py
@@ -4,15 +4,7 @@ url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/"
 
 inp = input('Enter command: ')
 a = inp.split()
-<<<<<<< HEAD
+b=a[1].split('/')
 resp = requests.delete(url + 'root' + a[1] + '.json').json()
-metadata_resp= requests.delete(url + 'metadata' + a[1] + '.json').json()
-=======
-resp = requests.delete(url + a[1] + '.json').json()
-<<<<<<< HEAD
-metadata_resp= requests.delete(url + '/metadata' + a[1] + ".json").json()
-=======
-metadata_resp= requests.delete(url + 'metadata/' + a[1] + '.json').json()
->>>>>>> e5989e5ee87cea889104eeac01509a4ee7fc53f9
+metadata_resp= requests.delete(url + 'metadata/' + b[-1] + '.json').json()
 
->>>>>>> a7dade83a0ca8f02035cab449f8e0a446bd8d706
