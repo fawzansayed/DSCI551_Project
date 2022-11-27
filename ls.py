1e59317739b4a8ebda520106c0a7c96365b56c0b
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2022-11-26 18:46:45 -0800

    replaced ls file

--- ls.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/root"
-
-inp=input('Enter command: ')
-a=inp.split()
-resp=requests.get(url+a[1]+'.json').json()
-print(resp)
