d574c1fad97f67dcf71c772b6563ca81a4d6deca
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2022-11-26 19:54:48 -0800

    Updated metadata update portion

--- EDFS/mkdir_firebase.py
+++ EDFS/mkdir_firebase.py
@@ -1,18 +1,25 @@
 import requests
+from datetime import datetime
 
 url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/"
 
-inp=input('Enter command: ')
-a=inp.split()
-new=a[1].split('/')[-1]
-dir=a[1].split('/')[1:-1]
-dire='root'
-if len(dir)!=0:
-    for a in dir:
-        dire=dire+'/'+a
+inp = input('Enter command: ')
+a = inp.split()
+new = a[1].split('/')[-1]
+dir = a[1].split('/')[1:-1]
+root_dir = 'root'
 
-if requests.get(url+dire+'/.json').status_code != 200 :
-    print('Directory not found!')
+# Establishing the metadata directory and getting the current time
+metadata = '/metadata'
+time = datetime.now().strftime('%m/%d/%Y')
+
+if len(dir) != 0:
+    for a in dir:
+        root_dir = root_dir + '/' + a
+
+if requests.get(url + root_dir + '/.json').status_code != 200 :
+    print('root_dirctory not found!')
     exit()
 
-resp = requests.patch(url+dire+'/.json',json={new:''})
+resp = requests.patch(url + root_dir + '/.json', json = {new:''})
+metadata_resp = requests.patch(url + metadata + '/.json', json = time)
