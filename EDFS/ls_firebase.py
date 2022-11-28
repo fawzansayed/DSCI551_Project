1d98a5e12de29255424ad9b1253ca84dab300a6e
gr8monk3ys <lorenzosca7@protonmail.ch>
2022-11-27 18:03:48 -0800

    Functionalized ls

--- EDFS/ls_firebase.py
+++ EDFS/ls_firebase.py
@@ -1,9 +1,14 @@
-import requests
 import json
+import requests
 
 url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/root"
 
-inp = input('Enter command: ')
-a = inp.split()
-resp = requests.get(url + a[1] + '.json').json()
-print(resp)
+def ls():
+    a = inp.split()
+    resp = requests.get(url + a[1] + '.json').json()
+
+    res = json.dump(resp)
+    return res
+
+if __name__ == '__main__':
+    ls()
