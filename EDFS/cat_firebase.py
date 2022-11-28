a7b010c24b6fd84d0ca662a22ac7d5c7a0568ac5
JeelMehta04 <jeelmaya@usc.edu>
2022-11-27 20:09:38 -0800

    Minor changes

--- EDFS/cat_firebase.py
+++ EDFS/cat_firebase.py
@@ -4,19 +4,22 @@ import pandas as pd
 
 url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/root"
 
-inp = input('Enter command: ')
-a = inp.split()
+def cat(inp):
+    a = inp.split()
+    
+    resp = requests.get(url + a[1] + '.json').json()
+    
+    first=0
+    for key, value in resp.items():
+        endpoint = value
+        _file = requests.get(value + '.json').json()
+        if first==0 :
+            df = pd.DataFrame(_file)
+        else :
+            df=df.append(pd.DataFrame(_file), ignore_index=True)
+        first=1
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df)
 
-resp = requests.get(url + a[1] + '.json').json()
-
-first=0
-for key, value in resp.items():
-    endpoint = value
-    _file = requests.get(value + '.json').json()
-    if first==0 :
-        df = pd.DataFrame(_file)
-    else :
-        df=df.append(pd.DataFrame(_file), ignore_index=True)
-    first=1
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(df)
+if __name__ == '__main__':
+    cat()
--- EDFS/rm_firebase.py
+++ EDFS/rm_firebase.py
@@ -2,9 +2,12 @@ import json
 import requests
 url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/"
 
-inp = input('Enter command: ')
-a = inp.split()
-b=a[1].split('/')
-resp = requests.delete(url + 'root' + a[1] + '.json').json()
-metadata_resp= requests.delete(url + 'metadata/' + b[-1] + '.json').json()
-
+def rm(inp):
+    a = inp.split()
+    b=a[1].split('/')
+    resp = requests.delete(url + 'root' + a[1] + '.json').json()
+    metadata_resp= requests.delete(url + 'metadata/' + b[-1] + '.json').json()
+    res="Successfully deleted {}".format(b)
+    return res
+if __name__ == '__main__':
+    rm()
