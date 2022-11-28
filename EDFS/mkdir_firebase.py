5be5f3f7e3795969a371defc80c270bd59521eff
gr8monk3ys <lorenzosca7@protonmail.ch>
2022-11-27 16:16:34 -0800

    funcitonalized mkdir

--- EDFS/mkdir_firebase.py
+++ EDFS/mkdir_firebase.py
@@ -3,23 +3,29 @@ from datetime import datetime
 
 url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/"
 
-inp = input('Enter command: ')
-a = inp.split()
-new = a[1].split('/')[-1]
-dir = a[1].split('/')[1:-1]
-root_dir = 'root'
+def mkdir(inp):
+    # inp = input('Enter command: ')
+    a = inp.split()
+    new = a[1].split('/')[-1]
+    _dir = a[1].split('/')[1:-1]
+    root_dir = 'root'
 
-# Establishing the metadata directory and getting the current time
-metadata = 'metadata'
-time = datetime.now().strftime('%m/%d/%Y')
+    # Establishing the metadata directory and getting the current time
+    metadata = 'metadata'
+    time = datetime.now().strftime('%m/%d/%Y')
 
-if len(dir) != 0:
-    for a in dir:
-        root_dir = root_dir + '/' + a
+    if len(_dir) != 0:
+        for a in _dir:
+            root_dir = root_dir + '/' + a
 
-if requests.get(url + root_dir + '/.json').status_code != 200 :
-    print('root_dirctory not found!')
-    exit()
+    if requests.get(url + root_dir + '/.json').status_code != 200 :
+        return 'Root dirctory not found'
+        exit()
 
-resp = requests.patch(url + root_dir + '/.json', json = {new:''})
-metadata_resp = requests.patch(url + metadata + '/.json', json = {new:time})
+    resp = requests.patch(url + root_dir + '/.json', json = {new:''})
+    metadata_resp = requests.patch(url + metadata + '/.json', json = {new:time})
+    res = 'Successfully created {} at {}'.format(new, time)
+    return res
+
+if __name__ == '__main__':
+    mkdir()
