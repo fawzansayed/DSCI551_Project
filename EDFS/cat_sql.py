adf2a1f91f78bd6c5a9f34c45dec73725b65aa5a
JeelMehta04 <jeelmaya@usc.edu>
2022-11-27 16:13:15 -0800

    Minor changes

--- EDFS/cat_sql.py
+++ EDFS/cat_sql.py
@@ -1,12 +1,18 @@
 import sqlite3
-
+import pandas as pd
 inp = input('Enter command: ')
 a = inp.split()
 temp=a[1].split('/')
 conn = sqlite3.connect('../DSCI551_Project.sqlite')
 cur = conn.cursor()
-cur.execute('SELECT * FROM  "{}" '.format(temp[-1].replace('"', '""')))
+data=cur.execute('SELECT * FROM  "{}" '.format(temp[-1].replace('"', '""')))
 r=cur.fetchall()
-print(r)
+for column in data.description:
+    c=column[0].to_list()
+df=pd.DataFrame(columns=c)
+for rows in r:
+    row=rows.split(',')
+    df.iloc[len(df)]=row
+print(df)
 conn.commit()
 conn.close()
