4699eba69de4ff9597b51c215864774412338292
fawzansayed <fsayed@usc.edu>
2022-11-27 16:37:27 -0800

    GPL and readPartition for firebase added

---
+++ EDFS/GPL_firebase.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/root"
+
+inp = input('Enter command: ')
+a = inp.split()
+resp = requests.get(url + a[1] + '.json').json()
+
+for k in resp.keys() :
+    print(k,': ',resp[k])
---
+++ EDFS/readPartition_firebase.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import pandas as pd
+
+url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/"
+
+inp = input('Enter command: ')
+a = inp.split()
+resp = requests.get(url + 'root' + a[1] + '.json').json()
+loc=resp['p'+a[2]]
+out=requests.get(loc + '.json').json()
+df = pd.DataFrame(out)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(df)
