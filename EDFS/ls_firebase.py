801549c160052c5babb1edb90ad51f26c945841d
gr8monk3ys <lorenzosca7@protonmail.ch>
2022-11-28 17:23:27 -0800

    Formatted script

--- EDFS/ls_firebase.py
+++ EDFS/ls_firebase.py
@@ -1,14 +1,15 @@
 import json
 import requests
 
-url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/root"
+url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/"
 
 def ls(inp):
     a = inp.split()
-    resp = requests.get(url + a[1] + '.json').json()
+    root = 'root'
 
-    res = json.dump(resp)
-    return res
+    resp = requests.get(url + root + a[1] + '.json').json()
+    res = json.dumps(resp , indent = 4)
+    return str(res)
 
 if __name__ == '__main__':
     ls()
