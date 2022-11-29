f80130a3b0b10939713268c2bbc1e601d400e474
gr8monk3ys <lorenzosca7@protonmail.ch>
2022-11-28 17:22:49 -0800

    Formatted script

--- EDFS/rm_firebase.py
+++ EDFS/rm_firebase.py
@@ -1,13 +1,17 @@
 import json
 import requests
+
 url = "https://dsci551project-1ff87-default-rtdb.firebaseio.com/"
 
 def rm(inp):
     a = inp.split()
-    b=a[1].split('/')
+    b = a[1].split('/')
+
     resp = requests.delete(url + 'root' + a[1] + '.json').json()
-    metadata_resp= requests.delete(url + 'metadata/' + b[-1] + '.json').json()
-    res="Successfully deleted {}".format(b)
+    metadata_resp = requests.delete(url + 'metadata/' + b[-1] + '.json').json()
+    res = "Successfully deleted {}".format(b[1])
     return res
+
+
 if __name__ == '__main__':
     rm()
